f6cf54c76c4d6f5dcd479c58d640ac52d2f54c2b
tratitude <tratitude@gmail.com>
2019-05-28 17:24:11 +0800

    pull merge

--- AS/urls.py
+++ AS/urls.py
@@ -4,10 +4,7 @@ from . import views
 
 urlpatterns = [
     path('test', views.current_datetime, name='current_datetime'),
-<<<<<<< HEAD
     path('login', views.login, name='login'),
     #path('index', views.index, name='index')
 
-=======
->>>>>>> 4ef92db0eef10847a2a5ca012e551d1494c2a5c3
 ]
