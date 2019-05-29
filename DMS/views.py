ffc0feea0d6bef48d3a41073a165999314f62aa8
Ridost <liao60209@gmail.com>
2019-05-29 13:57:04 +0800

    DormitoryApply

--- DMS/views.py
+++ DMS/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.conf import settings
+import datetime
 
+#from AS.model import DormiotoryApply
 # Create your views here.
+
 
 def hello(request):
     message = "Hello World!!"
@@ -10,4 +15,11 @@ def DMS(request):
     return render(request,"DMS/DMS.html",locals())
 
 def DormitoryApply(request):
+    start = settings.STARTTIME
+    end = settings.ENDTIME
+    now = datetime.datetime.now()
+    if now>start and now<end:
+        message = "有效"
+    else:
+        message = "無效"
     return render(request,"DMS/DormitoryApply.html",locals())
