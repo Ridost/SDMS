80f28526bc353b41a589be0edb7c3ed6b493aa8c
Ridost <liao60209@gmail.com>
2019-05-30 10:34:11 +0800

    DormitoryApply1

--- DMS/views.py
+++ DMS/views.py
@@ -15,11 +15,16 @@ def DMS(request):
     return render(request,"DMS/DMS.html",locals())
 
 def DormitoryApply(request):
+    #判斷是否為管理員
     start = settings.STARTTIME
     end = settings.ENDTIME
     now = datetime.datetime.now()
-    if now>start and now<end:
-        message = "有效"
-    else:
-        message = "無效"
-    return render(request,"DMS/DormitoryApply.html",locals())
+    if now>start and now<end:   #開放
+        #誰申請 哪個系 年級 學號 姓名 哪一棟宿舍 申請時間 審核狀態
+        #宿舍房間狀況 eg. OF 5層樓 20個房間 4個人
+        return render(request, "DMS/DormitoryApply.html", locals())
+    else:   #不開放
+        return render(request, "DMS/DMS.html", locals())
+
+
+
