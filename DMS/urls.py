1c7e82f5f7ac70108fdb6ebbf1920e153d63ec5f
Ridost <liao60209@gmail.com>
2019-05-31 23:13:44 +0800

    AccountCreate

--- DMS/urls.py
+++ DMS/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('DMS',views.DMS, name = 'DMS'),
     path('DormitoryApply', views.DormitoryApply),
     path('DormCheck', views.DormCheck),
+    path('StudentCreate', views.StudentCreate),
+    path('AccountCreate', views.AccountCreate),
 ]
