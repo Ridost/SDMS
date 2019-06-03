67c255164ab4277114287f891924790c65de99ea
soimportant <soimportant0318@gmail.com>
2019-06-03 23:05:09 +0800

    fix conflict

--- DLS/admin.py
+++ DLS/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from AS.models import Billboard
-# Register your models here.
-
-admin.site.register(Billboard)
-=======
 from AS.models import Billboard, StudentInfo, Equipment, BorrowRecord
 # Register your models here.
 
@@ -12,4 +6,3 @@ admin.site.register(Billboard)
 admin.site.register(StudentInfo)
 admin.site.register(Equipment)
 admin.site.register(BorrowRecord)
->>>>>>> DLS
