fc554ea06f45e1c7960d375b05391a786b17f2aa
Ridost <liao60209@gmail.com>
2019-06-03 21:06:39 +0800

    DormCheck_Admin

--- DMS/views.py
+++ DMS/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from AS.models import Account,StudentInfo,DormRecord,DormInfo
 from django.db import transaction
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 import datetime,random
 
 #from AS.model import DormiotoryApply
@@ -32,12 +33,36 @@ def DormitoryApply(request):
         #宿舍房間狀況 eg. OF 5層樓 20個房間 4個人
         return render(request, "DMS/DormitoryApply.html", locals())
     else:   #不開放
+        message = "目前並非宿舍申請時間"
         return render(request, "DMS/DMS.html", locals())
 
 @login_required(login_url='/AS/login/')
 def DormCheck(request):
     name = request.user.username
-    if request.method=='POST':
+    account = Account.objects.get(user=request.user)
+    if  account.permission==0:  #管理員
+        DormRecords = DormRecord.objects.all()
+        dorms = []
+        for dormRecord in DormRecords:
+            try:
+                dorm = DormInfo.objects.get(account=dormRecord.account)
+                result = dorm.building+dorm.room+dorm.bed
+            except:
+                now = datetime.datetime.now()
+                end = settings.ENDTIME
+                if now>end:
+                    result = "銘謝惠顧"
+                else:
+                    result = "尚未開始分發"
+            dm = {
+                'D1': DormRecord.Dorms[dormRecord.order1][1],
+                'D2': DormRecord.Dorms[dormRecord.order2][1],
+                'D3': DormRecord.Dorms[dormRecord.order3][1],
+                'result': result
+            }
+            dorms.append(dm)
+        return render(request, "DMS/DormCheck.html", locals())
+    elif request.method=='POST':    #學生
         name = request.user.username
         D1 = request.POST['Dorm1']
         D2 = request.POST['Dorm2']
@@ -46,7 +71,6 @@ def DormCheck(request):
             error = "志願序不可重複"
             return render(request, "DMS/DormitoryApply.html",locals())
         else:
-            account = Account.objects.get(user=request.user)
             try:
                 student = StudentInfo.objects.get(account = account)
             except:
@@ -84,6 +108,14 @@ def DormCheck(request):
         except:
             error = "此用戶並非學生"
             return render(request, "DMS/DMS.html", locals())
+        dm = {
+            'D1':D1,
+            'D2':D2,
+            'D3':D3,
+            'result':result
+        }
+        dorms = []
+        dorms.append(dm)
         return render(request,"DMS/DormCheck.html",locals())
 
 @login_required(login_url='/AS/login/')
@@ -97,4 +129,6 @@ def DormDelete(request):
     except:
         error = "查無此資料!!!"
         return render(request,"DMS/DMS.html",locals())
+
+
 
