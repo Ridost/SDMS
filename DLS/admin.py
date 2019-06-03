31b41cde7d8904e7e7ddf250c0de3ecd31db3750
soimportant <soimportant0318@gmail.com>
2019-06-03 19:34:03 +0800

    fix

--- DLS/admin.py
+++ DLS/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from AS.models import Billboard, StudentInfo, Equipment, BorrowRecord
 # Register your models here.
 
@@ -7,10 +6,3 @@ admin.site.register(Billboard)
 admin.site.register(StudentInfo)
 admin.site.register(Equipment)
 admin.site.register(BorrowRecord)
-=======
-from AS.models import Billboard, StudentInfo
-# Register your models here.
-
-admin.site.register(Billboard)
-admin.site.register(StudentInfo)
->>>>>>> 4b7e473f7b61da3759f131302d136c6d6d6c80f4
--- DLS/urls.py
+++ DLS/urls.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from django.urls import path, re_path
-=======
-from django.urls import path
->>>>>>> 4b7e473f7b61da3759f131302d136c6d6d6c80f4
 from django.conf.urls import url
 from .views import *
 
@@ -11,14 +7,10 @@ urlpatterns = [
     path('addbillboard', AddBillboard),
     path('deletebillboard', DeleteBillboard),
     path('modifybillboard', ModifyBillboard),
-<<<<<<< HEAD
 
     path('package', ShowPackage),
     path('package/manage', ManagePackage),
     path('package/verify', VerifyPackage),
     re_path(r'^package/modify/?([0-9]*)/$', ModifyPackage),
     path('package/delete', DeletePackage),
-=======
-    path('package', ShowPackage)
->>>>>>> 4b7e473f7b61da3759f131302d136c6d6d6c80f4
 ]
