ab8d4ed6bd3aa5a6c4447dc48c9176b9b5f71a8e
Ridost <liao60209@gmail.com>
2019-06-04 21:17:01 +0800

    DormRecordCreate

--- DMS/urls.py
+++ DMS/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from DMS import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello'),
     path('main/',views.DMS, name = 'main'),
     path('DormitoryApply/', views.DormitoryApply),
     path('DormCheck/', views.DormCheck),
     path('DormDelete/', views.DormDelete),
+    path('DormRecordCreate', views.DormRecordCreate),
 ]
