9dd6b2e065df0c2aee79eac344b4cd063b7ed532
soimportant <soimportant0318@gmail.com>
2019-06-12 02:53:24 +0800

    DLS function done, include css

--- DLS/urls.py
+++ DLS/urls.py
@@ -3,17 +3,24 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    path('billboard', ShowBillboard),
-    path('addbillboard', AddBillboard),
-    path('deletebillboard', DeleteBillboard),
-    path('modifybillboard', ModifyBillboard),
+    path('billboard/', ShowBillboard),
+    path('addbillboard/', AddBillboard),
+    path('deletebillboard/', DeleteBillboard),
+    path('modifybillboard/', ModifyBillboard),
 
-    path('package', ShowPackage),
-    path('package/manage', ManagePackage),
-    path('package/verify', VerifyPackage),
+    path('package/', ShowPackage),
+    path('package/manage/', ManagePackage),
+    path('package/verify/', VerifyPackage),
     re_path(r'^package/modify/?([0-9]*)/$', ModifyPackage),
-    path('package/delete', DeletePackage),
+    path('package/delete/', DeletePackage),
 
-    path('borrow', BorrowSpace),
-    path('borrow/check', CheckSpace)
+    path('borrow/showall/', ShowRecord),
+    path('borrow/manage/', ManageRecord),
+    path('borrow/manage/confirm/', ConfirmRecord),
+    path('borrow/manage/withdraw/', WithdrawRecord),
+
+    path('borrow/apply/', BorrowSpace),
+    path('borrow/status/', ShowStatus),
+    path('borrow/check/', CheckSpace),
+    
 ]
