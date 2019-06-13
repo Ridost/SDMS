75c25733de1ecdd36d161b809291d102aa25b9cb
Yabadado <jacky992008@gmail.com>
2019-06-13 17:55:41 +0800

    unknown conflict after pull

--- DLS/admin.py
+++ DLS/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from AS.models import Billboard
-# Register your models here.
-
-admin.site.register(Billboard)
-=======
-from AS.models import Billboard, StudentInfo, Equipment, BorrowRecord
+from AS.models import Billboard, Equipment, BorrowRecord
 # Register your models here.
 
 admin.site.register(Billboard)
 admin.site.register(StudentInfo)
 admin.site.register(Equipment)
 admin.site.register(BorrowRecord)
->>>>>>> DLS
