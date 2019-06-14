c491ce02eecefd76a690a8a64161c888c44e806b
fdmdkw <lk1919618@gmail.com>
2019-06-14 17:18:31 +0800

    generate db schema diagram by django_extensions

--- SDMS/settings.py
+++ SDMS/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'DLS.apps.DlsConfig',
     'DMS.apps.DmsConfig',
     'RS.apps.RsConfig',
+    #'django_extensions',
+    'django.contrib.sitemaps',
+    'django.contrib.sites'
 ]
 
 MIDDLEWARE = [
