48b6827a5e64e52274a732e7d678c3815b5fdc11
soimportant <soimportant0318@gmail.com>
2019-06-15 16:48:21 +0800

    change some url

--- DLS/urls.py
+++ DLS/urls.py
@@ -4,17 +4,16 @@ from .views import *
 
 urlpatterns = [
     path('billboard/', ShowBillboard),
-    path('addbillboard/', AddBillboard),
-    path('deletebillboard/', DeleteBillboard),
-    path('modifybillboard/', ModifyBillboard),
+    path('billboard/add/', AddBillboard),
+    path('billboard/delete/', DeleteBillboard),
+    path('billboard/modify/', ModifyBillboard),
 
     path('package/', ShowPackage),
     path('package/manage/', ManagePackage),
     path('package/verify/', VerifyPackage),
-    re_path(r'^package/modify/?([0-9]*)/$', ModifyPackage),
-    path('package/delete/', DeletePackage),
 
-    path('borrow/showall/', ShowRecord),
+
+    path('borrow/manage/showall', ShowRecord),
     path('borrow/manage/', ManageRecord),
     path('borrow/manage/confirm/', ConfirmRecord),
     path('borrow/manage/withdraw/', WithdrawRecord),
